84b1fd06c4509f95a8f01d074697eb819b42b9c9
Buga Iurii <bugaiurii@yandex.ru>
2022-09-23 10:35:46 +0300

    step 3

--- ex1.py
+++ ex1.py
@@ -1,10 +1,9 @@
-i = 1
-x = 1
-
-while x != 0:
-    x = int(input())
-    if x > 0:
-        i = i * x
-    elif x < 0:
-        continue
-print(i)
+city = input().split()
+i = 0
+while i < len(city):
+    if len(city[i]) < 6:
+        print('НЕТ')
+        break
+    i += 1
+else:
+    print('ДА')
