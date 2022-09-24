e678b1db6075f457178e409a759157a1f1d07b35
Buga Iurii <bugaiurii@yandex.ru>
2022-09-24 12:10:22 +0300

    step 6

--- ex1.py
+++ ex1.py
@@ -1,19 +1,8 @@
 n = int(input())
 i = 1
-mylist = []
 
-if n >= 100:
-    print(f'слишком большое значение n')
-else:
-    while i <= n:
-        if i % 3 == 0 and i % 5 == 0:
-            mylist.append(i)
-        i += 1
-    else:
-        print(*mylist)
-
-
-
-
-
-
+while True:
+    if i**2 > n:
+        print(i)
+        break
+    i += 1
